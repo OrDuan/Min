c130e6486d302d02884341283f55639f5ffeeea6
Or Duan <orduani@gmail.com>
2014-04-06 17:15:24 +0300

    index posts nav

--- index.php
+++ index.php
@@ -13,25 +13,27 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php $custom_query = new WP_Query( 'posts_per_page=3' ); ?>
+		<?php $paged = (get_query_var('page')) ? get_query_var('page') : 1; ?>
+		<?php query_posts( $custom_query . "&page=$page" ); ?>
 
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( $custom_query->have_posts() ) :  $custom_query->the_post(); ?>
 
 				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
+				get_template_part( 'content', get_post_format() );
 				?>
 
 			<?php endwhile; ?>
 
-			<?php min_paging_nav(); ?>
+			<nav id="nav-below">
+				<?php next_posts_link('&laquo; Older Entries', $custom_query->max_num_pages) ?>
+			</nav><!-- #nav-below -->
 
 		<?php else : ?>
 
@@ -39,7 +41,10 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php  wp_reset_query(); // Reset Query ?> 
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
 
 <?php get_footer(); ?>
